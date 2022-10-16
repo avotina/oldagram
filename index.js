3e9a95c724d8d75d09b0af7d11e3bdc86ba76c6c
Santa <avotina@protonmail.com>
2022-10-16 22:47:47 +0300

    exported data

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21492
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4567
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import posts from "/data.js"
 
 const containers = document.getElementById("containers")
     
@@ -72,6 +44,6 @@ renderPosts()
 
 document.getElementById("likes-btn").addEventListener("click", function() {
         //   console.log("liked")
-        let likes = document.getElementById("likes").innerHTML = 
-        console.log(likes)
+        
+        console.log(`${post.likes +=1}`)
     })
